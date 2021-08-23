c4568e111c534593a585622e7b3b983659044db5
Gustavo <gustavo@obex.pw>
2021-08-23 15:36:53 -0300

    added more test cases

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const Biscoint = require("../dist").default;
 
 const expect = require("chai").expect;
 
-const base = "BTC",
+const base = "ETH",
   quote = "BRL";
 
 const api = new Biscoint({
@@ -44,7 +44,6 @@ describe("public endpoints", () => {
   });
 });
 
-/*
 describe("private endpoints", () => {
   it("should receive balances", async () => {
     const balance = await api.balance();
@@ -52,5 +51,66 @@ describe("private endpoints", () => {
       expect(balance[iterator]).to.be.a("string");
     }
   });
+
+  it(`should request an ${base} offer`, async () => {
+    const offer = await api.offer({
+      base,
+      amount: 50,
+      isQuote: true,
+      op: "buy",
+    });
+
+    expect(offer).to.have.all.keys(
+      "offerId",
+      "base",
+      "quote",
+      "op",
+      "isQuote",
+      "baseAmount",
+      "quoteAmount",
+      "efPrice",
+      "createdAt",
+      "expiresAt",
+      "apiKeyId"
+    );
+
+    expect(offer.base).to.eq("ETH");
+  });
 });
-*/
+
+describe("get and confirm offer test suit", () => {
+  let offer;
+
+  before(async () => {
+    offer = await api.offer({
+      base,
+      amount: 50,
+      isQuote: true,
+      op: "buy",
+    });
+  });
+
+  it("confirm offer", async () => {
+    const confirmOffer = await api.confirmOffer({
+      offerId: offer.offerId,
+    });
+
+    expect(confirmOffer).to.have.all.keys(
+      "offerId",
+      "base",
+      "quote",
+      "op",
+      "isQuote",
+      "baseAmount",
+      "quoteAmount",
+      "efPrice",
+      "createdAt",
+      "confirmedAt",
+      "apiKeyId"
+    );
+
+    expect(offer.base).to.be.eq(confirmOffer.base);
+    expect(offer.baseAmount).to.be.eq(confirmOffer.baseAmount);
+    expect(offer.quoteAmount).to.be.eq(confirmOffer.quoteAmount);
+  });
+});
